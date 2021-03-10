899914e66dd36851393db13e71a05397b5f2865a
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-03-10 11:14:03 +0000

    Server setup

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 module.exports = app;
 
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json);
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, function() {
+    console.log(`Server is listening on ${PORT}.`)
+  })
 }
